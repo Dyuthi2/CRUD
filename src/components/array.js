3d8ff0bcbd329a6e5ecb79b8db9b1a58ef302c8d
Dyuthi2 <dyuthi.dinesh@saama.com>
2022-07-03 23:47:28 +0530

    file passed

---
+++ src/components/array.js
@@ -0,0 +1,19 @@
+const array = [
+  {
+    id: "1",
+    Name: "Shivansh",
+    Age: "23",
+  },
+  {
+    id: "2",
+    Name: "Kajal",
+    Age: "22",
+  },
+  {
+    id: "3",
+    Name: "Amal",
+    Age: "23",
+  },
+];
+
+export default array;
