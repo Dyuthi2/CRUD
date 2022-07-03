b41ed17817ae10b939c6cbe4d38abf581aae412f
Dyuthi2 <85401090+Dyuthi2@users.noreply.github.com>
2022-07-04 00:24:29 +0530

    Update Create.js

--- src/components/Create.js
+++ src/components/Create.js
@@ -6,36 +6,32 @@ import { v4 as uuid } from "uuid";
 import { Link, useNavigate } from "react-router-dom";
 
 function Create() {
-  // Making usestate for setting and
-  // fetching a value in jsx
+  
   const [name, setname] = useState("");
   const [age, setage] = useState("");
 
-  // Using useNavigation for redirecting to pages
+
   let history = useNavigate();
 
-  // Function for creating a post/entry
+  
   const handelSubmit = (e) => {
-    e.preventDefault(); // Prevent reload
+    e.preventDefault(); 
 
-    const ids = uuid(); // Creating unique id
-    let uni = ids.slice(0, 8); // Slicing unique id
-
-    // Fetching a value from usestate and
-    // pushing to javascript object
+    const ids = uuid(); 
+    let uni = ids.slice(0, 8); 
+   
     let a = name,
       b = age;
     array.push({ id: uni, Name: a, Age: b });
 
-    // Redirecting to home page after creation done
+    
     history("/");
   };
 
   return (
     <div>
       <Form className="d-grid gap-2" style={{ margin: "15rem" }}>
-        {/* Fetching a value from input textfirld
-in a setname using usestate*/}
+        
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Control
             onChange={(e) => setname(e.target.value)}
@@ -45,8 +41,7 @@ in a setname using usestate*/}
           />
         </Form.Group>
 
-        {/* Fetching a value from input textfirld in
-	a setage using usestate*/}
+       
         <Form.Group className="mb-3" controlId="formBasicAge">
           <Form.Control
             onChange={(e) => setage(e.target.value)}
@@ -56,8 +51,7 @@ in a setname using usestate*/}
           />
         </Form.Group>
 
-        {/* handing a onclick event in button for
-	firing a function */}
+       
         <Button
           onClick={(e) => handelSubmit(e)}
           variant="primary"
@@ -66,7 +60,7 @@ in a setname using usestate*/}
           Submit
         </Button>
 
-        {/* Redirecting back to home page */}
+       
         <Link className="d-grid gap-2" to="/">
           <Button variant="info" size="lg">
             Home
